3dfad2df60930f4ecd4efe6b81bc2d2563bd1197
SAEStudentNathanHoward <114543371+SAEStudentNathanHoward@users.noreply.github.com>
2022-11-09 11:55:57 +1100

    Finalised most of the code and added a blood splatter that appears

--- Assets/Scripts/Crewmates.cs
+++ Assets/Scripts/Crewmates.cs
@@ -9,8 +9,13 @@ namespace NathanHoward
 {
     public class Crewmates : MonoBehaviour
     {
-        List<string> possibleCrewNames = new List<string>();
-        List<string> possibleCrewHobbies;
+        // Creation of my lists
+        [SerializeField] List<string> possibleCrewNames = new List<string>();
+        [SerializeField] List<string> possibleCrewHobbies = new List<string>();
+        [SerializeField] List<string> possibleAlienHobbies = new List<string>();
+
+        //Linking of the Ship script
+        public Ship shipScript;
 
         //Variables used to detirmine if the crewmate is an alien
         private int alienTemp;
@@ -30,17 +35,26 @@ namespace NathanHoward
         
         private void Start()
         {
-            CreateCrewmate(Random.Range(1, 50), Random.Range(1, 50));
+            CreateCrewmate(Random.Range(1, 30), Random.Range(1, 30));
         }
 
+        // The method that controls the creation of the crewmates
+        // Needs 2 numbers passed into it
         private void CreateCrewmate(int crewNameGrab, int crewHobbyGrab)
         {
-            alienTemp = Random.Range(1, 2);
-            if (alienTemp == 1)
+            // Coin flip to detirmine of the proposed crewmate is an Alien
+            alienTemp = Random.Range(1, 20);
+            
+            // Checking the outcome of the coin flip
+            if (alienTemp >= 15)
             {
                 isAlien = true;
-                
 
+                // Grabbing the created crewmates name and hobby
+                newCrewmateName = possibleCrewNames[crewNameGrab];
+                newCrewmateHobby = possibleAlienHobbies[crewHobbyGrab];
+                
+                // Setting the UI text to display the name and hobby
                 crewNameText.text = newCrewmateName;
                 crewHobyText.text = newCrewmateHobby;
                 
@@ -48,20 +62,38 @@ namespace NathanHoward
             else
             {
                 isAlien = false;
+                
+                // Grabbing the created crewmates name and hobby
+                newCrewmateName = possibleCrewNames[crewNameGrab];
+                newCrewmateHobby = possibleCrewHobbies[crewHobbyGrab];
 
+                // Setting the UI text to display the name and hobby
+                crewNameText.text = newCrewmateName;
+                crewHobyText.text = newCrewmateHobby;
             }
         }
 
+        // The method called when the "Reject" button is pressed
         public void RejectCrewmate()
         {
-            Debug.Log("REJECT CREWMATE");
+            //Debug.Log("REJECT CREWMATE");
+
+            // Resets the alien variable and creates a new crewmate
             isAlien = false;
+            CreateCrewmate(Random.Range(1, 30), Random.Range(1, 30));
         }
 
+        // The method called when the "Hire" button is pressed
         public void HireCrewmate()
         {
-            Debug.Log("HIRE CREWMATE");
+            //Debug.Log("HIRE CREWMATE");
+
+            // Sending the variable to the Ship script and passing the required values
+            shipScript.AddToShip(isAlien, newCrewmateName, newCrewmateHobby);
+
+            // Resets the alien variable and creates a new crewmate
             isAlien = false;
+            CreateCrewmate(Random.Range(1, 30), Random.Range(1, 30));
         }
     }
 }
--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -7,28 +7,82 @@ namespace NathanHoward
     public class Ship : MonoBehaviour
     {
         //Creation of the 2 lists
-        List<string> currentCrewNames;
-        List<string> currentCrewHobbies;
+        [SerializeField] List<string> currentCrewNames;
+        [SerializeField] List<string> currentCrewHobbies;
 
-        //public method to add crewmembers to the ship
+        [SerializeField] GameObject mainCanvas;
+        [SerializeField] GameObject bloodSplatter;
+
+        private int currentCrewCount = 0;
+
+        private void Update()
+        {
+            if (bloodSplatter.GetComponent<CanvasGroup>().alpha > 0f)
+            {
+                bloodSplatter.GetComponent<CanvasGroup>().alpha -= 0.001f;
+            }
+        }
+
+        // Public method to add crewmembers to the ship
         public void AddToShip(bool isAlien, string newCrewName, string newCrewHobby)
         {
-            //Checking to see if the new crew member is an alien
+            // Checking to see if the new crew member is an alien
             if (isAlien == false)
             {
-                //Add the new crew member to the list along with their hobby
+                // Add the new crew member to the list along with their hobby
                 currentCrewNames.Add(newCrewName);
                 currentCrewHobbies.Add(newCrewHobby);
+
+                currentCrewCount += 1;
+
+                //Debug.Log(currentCrewCount);
+
+                if (currentCrewCount == 10)
+                {
+                    mainCanvas.SetActive(false);
+                }
             }
             else
             {
-                //Code that calls the killing of a random amount of Crew Members
+                // Code that calls the killing of a random amount of Crew Members
                 EliminateCrewmates(Random.Range(1,3));
             }
         }
+
+        // Method used to Destroy the Crewmates
+        // Needs input from AddToShip
         private void EliminateCrewmates(int amountKilled)
         {
+            
+            //Debug.Log(amountKilled);
+
+            // Sifting through to delete a random number of crewmates
+            for (int i = 0; i < amountKilled; i++)
+            {
+                // Creation of a random int to track which crewmate to delete
+                int currentCrewIndex = Random.Range(1, currentCrewCount);
+                
+                //Debug.Log(currentCrewIndex);
+               
+                // Removal of the crew names and hobbies
+                currentCrewNames.RemoveAt(currentCrewIndex);
+                currentCrewHobbies.RemoveAt(currentCrewIndex);
+
+                //Debug.Log("End of for i");
+                //Debug.Log("i = " + i);
+
+                currentCrewCount -= amountKilled;
+                
+                //Debug.Log(currentCrewCount);
+                bloodSplatter.GetComponent<CanvasGroup>().alpha = 1;
+            }
+
             Debug.Log("Alien Detected! You have just lost " + amountKilled + " crewmates!");
+        }
+
+        public void ShowCrewmates()
+        {
+
         }
     }
 }
